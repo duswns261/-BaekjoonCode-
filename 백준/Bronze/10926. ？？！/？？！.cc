f2ab8981ab0aa2d512523fd3b093602431bfa70a
Yeonjun.Jeong <41406855+duswns261@users.noreply.github.com>
2022-09-27 23:44:18 +0900

    [Bronze V] Title: ??!, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    char strSpecial[51];
+    scanf("%s", strSpecial);
+    printf("%s??!", strSpecial);
+    return 0;
+}
